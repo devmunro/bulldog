9ed7be635f0bbd620f3b30f47e5e5602996d7da0
David Munro <defmunro@gmail.com>
2023-01-21 13:53:42 +0000

    add api for getALLExercises

--- SERVER/controllers/exerciseController.js
+++ SERVER/controllers/exerciseController.js
@@ -4,14 +4,17 @@ import axios from 'axios';
 const apiKey = process.env.NINJA_API_KEY;
 
 
-
-
 export const getAllExercises = ( req, res) => {
-
     const url = 'https://api.api-ninjas.com/v1/exercises';
     const headers = {
         'X-Api-Key': apiKey
-    
-}
-
+    };
+    axios.get(url, {headers: headers})
+    .then(response => {
+        res.status(200).json(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    });
 }
