dc6b72bcd776200aa24c7e0b59d5fb80b1476a12
David Munro <defmunro@gmail.com>
2023-01-21 13:41:28 +0000

    getallexercises contoller added

--- SERVER/controllers/exerciseController.js
+++ SERVER/controllers/exerciseController.js
@@ -1,7 +1,15 @@
+import 'dotenv/config';
+const apiKey = process.env.NINJA_API_KEY;
+
 
 
 
 export const getAllExercises = ( req, res) => {
 
+    const url = 'https://api.api-ninjas.com/v1/exercises' + muscle;
+    const headers = {
+        'X-Api-Key': apiKey
     
+}
+
 }
--- SERVER/routes/exerciseRoutes.js
+++ SERVER/routes/exerciseRoutes.js
@@ -1,4 +1,5 @@
 import express from "express"
+import { getAllExercises } from "../controllers/exerciseController.js"
 
 
 export const exerciseRoutes = () => {
