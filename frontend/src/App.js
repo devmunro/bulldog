64290025478193b6a4bcccc0312b22387befd918
David Munro <defmunro@gmail.com>
2023-01-23 21:22:54 +0000

    added react router and dashboard file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
     <div className="App">
+      
+      <Router>
+        <Route path='/Dashboard' element={<Dashboard/>}></Route>
+      </Router>
      
     </div>
   );
