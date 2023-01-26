1478a9709f0fdd6e63f4490550ebec39bf559837
David Munro <defmunro@gmail.com>
2023-01-26 11:27:19 +0000

    added basis exercise and user reducer

---
+++ frontend/src/features/exerciseSlice.js
@@ -0,0 +1,10 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+export const exerciseSlice = createSlice({
+   
+
+})
+
+export default exerciseSlice.reducer;
