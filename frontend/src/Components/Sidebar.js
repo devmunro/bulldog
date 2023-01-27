b0d21b7da3ec904e31aa1490103f150be87c1538
David Munro <defmunro@gmail.com>
2023-01-27 18:33:43 +0000

    add link to workout and overview, hid future dashboard oages

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -5,6 +5,8 @@ import {
   UsersIcon,
 } from "@heroicons/react/24/solid";
 import { ChartBarIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
+
 
 export default function Sidebar() {
   return (
@@ -12,21 +14,25 @@ export default function Sidebar() {
       <h1 className="text-white m-6 text-center font-bold">BULLDOG</h1>
       <ul className="mt-10 ml-5 space-y-3 text-sm text-left [&>*]:p-2  [&>*]:rounded-l-2xl [&>*]:flex">
         <li className="hover:bg-[#2B2946] hover:text-white">
+          <Link to="/dashboard" className="flex">
           <ComputerDesktopIcon className="h-6 w-6 text-white-500 mx-2" />
           Overview
+          </Link>
         </li>
-        <li className="hover:bg-[#2B2946] hover:text-white">
+        {/* <li className="hover:bg-[#2B2946] hover:text-white">
           <UsersIcon className="h-6 w-6 text-white-500 mx-2" />
           Social
-        </li>
+        </li> */}
         <li className="hover:bg-[#2B2946] hover:text-white">
+          <Link to="/dashboard/workout" className="flex">
           <TrophyIcon className="h-6 w-6 text-white-500 mx-2" />
           Workout
+          </Link>
         </li>
-        <li className="hover:bg-[#2B2946] hover:text-white">
+        {/* <li className="hover:bg-[#2B2946] hover:text-white">
           <ChartBarIcon className="h-6 w-6 text-white-500 mx-2" />
           Stats
-        </li>
+        </li> */}
       </ul>
     </div>
   );
