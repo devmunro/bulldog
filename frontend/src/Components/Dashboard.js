97a26dc8c0c86330c08bff6e7fe1cd3ea9ac4f11
David Munro <defmunro@gmail.com>
2023-01-28 11:14:03 +0000

    add catergory for exercises

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -8,7 +8,7 @@ import Overview from "./Overview";
 
 export default function Dashboard() {
   return (
-    <div className="bg-[#2B2946] h-full flex w-screen">
+    <div className="bg-[#2B2946] h-full flex w-full">
       <Sidebar />
       <div className="flex-col w-full ml-20">
         <Navbar />
--- frontend/src/Components/Workout.js
+++ frontend/src/Components/Workout.js
@@ -5,6 +5,15 @@ import { fetchExercise } from "../features/exerciseSlice";
 export default function Workout() {
   const dispatch = useDispatch();
 
+  const catergories = [
+    "cardio",
+    "olympic weightlifting",
+    "plyometrics",
+    "powerlifting",
+    "strength",
+    "stretching",
+    "strongman",
+  ];
   const [exerciseList, setExerciseList] = useState();
   const [loading, setLoading] = useState(true);
 
@@ -22,19 +31,30 @@ export default function Workout() {
   //
   return (
     <div>
-      <section className="flex flex-wrap max-w-full">
-      {!loading &&
-        exerciseList.map((exercise) => {
-          return (
-            <ul key={exercise.name} className="text-white bg-black w-[30%] m-2 p-4">
-              <li>{exercise.name}</li>
-              <li>{exercise.type}</li>
-              <li>{exercise.difficulty}</li>
-              <li>{exercise.instructions}</li>
-            </ul>
-          );
+      <div className="bg-black text-white max-w-full m-2">
+       <h2 className="font-bold">Catergories</h2>
+       <ul className="flex gap-2 w-full ">
+        {catergories.map((item) => {
+          return (<li className=" w-full">{item}</li>);
         })}
-        </section>
+        </ul>
+      </div>
+      <section className="flex flex-wrap max-w-full bg-black">
+        {!loading &&
+          exerciseList.map((exercise) => {
+            return (
+              <ul
+                key={exercise.name}
+                className="text-white bg-[#2B2946] w-[30%] m-2 p-4"
+              >
+                <li>{exercise.name}</li>
+                <li>{exercise.type}</li>
+                <li>{exercise.difficulty}</li>
+                <li>{exercise.instructions}</li>
+              </ul>
+            );
+          })}
+      </section>
     </div>
   );
 }
