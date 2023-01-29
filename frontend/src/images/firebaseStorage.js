2e1cbbf9be75c4fccba8c15b6d9571f884ee4688
David Munro <defmunro@gmail.com>
2023-01-29 16:02:35 +0000

    firebase storage added and icons added for categories

---
+++ frontend/src/images/firebaseStorage.js
@@ -0,0 +1,23 @@
+import { storage } from "../firebaseConfig";
+import { ref, getDownloadURL } from "firebase/storage";
+import { useState } from "react";
+
+
+export default function FirebaseStorage({imageBase}) {
+
+  const [image, setImage] = useState("");
+    const imageURL = `icons/${imageBase}`
+  // Create a child reference
+  const imagesRef = ref(storage, imageURL);
+  // imagesRef now points to 'images'
+if(imageBase !== "")
+  // Get the download URL
+  getDownloadURL (imagesRef)
+    .then((url) => {
+      setImage(url);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return <img className="h-16 w-16" src={image} alt="thing"/>
+}
