288105d39a1c99d71233db38b1e00187b08c173c
David Munro <defmunro@gmail.com>
2023-02-02 14:27:15 +0000

    add user registration and tested

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import RegisterForm from './UserForms/userRegistration'
 
 export default function Home() {
   return (
-    <div className="bg-black">
-      Home
+    <div className='h-screen' >
+      
+      <div className="bg-[#2B2946] h-1/2" ></div>
+      <div className="bg-black h-1/2"></div>
+      <div className='absolute bg-white inset-40 ring-4 shadow-2xl	'>
+
+        <RegisterForm/>
+      </div>
     </div>
   )
 }
--- frontend/src/features/userSlice.js
+++ frontend/src/features/userSlice.js
@@ -1,12 +1,53 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+//API LINK
+const API_URL = "/api/";
+
+// fetch Exercises
+export const registerUser = createAsyncThunk(
+  "auth/regUser",
+  async (userdata) => {
+    try {
+      const response = await axios.post(`${API_URL}signup`, userdata);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 export const userSlice = createSlice({
-   
- 
-    name: 'user',
-    initialState: [],
-})
+  name: "auth",
+  initialState: {
+    userData: null,
+    loading: false,
+    error: null,
+    success: false,
+  },
 
+  reducers: {
+    clearState: (state) => {
+      state.error = null;
+      state.loading = false;
+      state.success = false;
+    },
+  },
+  extraReducers: {
+    [registerUser.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [registerUser.fulfilled]: (state, action) => {
+      state.userData = action.payload;
+      state.loading = false;
+      state.success = true;
+    },
+    [registerUser.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    },
+  },
+});
+
+export const { clearState } = userSlice.actions;
 export default userSlice.reducer;
