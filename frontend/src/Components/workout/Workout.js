67a096975fdb870c3daf74f6b219ab85c4102709
David Munro <defmunro@gmail.com>
2023-02-06 17:04:00 +0000

    add simple button

--- frontend/src/Components/workout/Workout.js
+++ frontend/src/Components/workout/Workout.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Workout() {
   return (
-    <div>
-      <button>Create Workout</button>
+    <div className='bg-white'>
+      <button className='border-2 border-gray-400 px-2 m-2'>Create Workout</button>
     </div>
   )
 }
