eb8f6335ab2016f7b5dd27e998a9e4b3a8b2b9d9
David Munro <defmunro@gmail.com>
2023-02-06 12:34:21 +0000

    updates

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginForm from "./UserForms/userLogin";
 import RegisterForm from "./UserForms/userRegistration";
 
 export default function Home() {
+
+
+  const [showform, setShowForm] = useState(true)
+const handleClick = (e) => {
+e.preventDefault()
+
+setShowForm(!showform)
+
+}
+
   return (
     <div className="h-screen">
       <div className="bg-[#2B2946] h-1/2"></div>
       <div className="bg-black h-1/2"></div>
       <div className="absolute bg-blue-500 inset-40 ring-4 shadow-2xl flex	">
-        <RegisterForm />
-        <LoginForm />
+        <button onClick={handleClick} className="bg-black">login</button>
+        {!showform && <RegisterForm />}
+      {showform &&  <LoginForm />}
       </div>
     </div>
   );
