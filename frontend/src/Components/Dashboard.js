ac034541b4056f74de807d2b8c3be25724381599
David Munro <defmunro@gmail.com>
2023-02-07 12:38:50 +0000

    add useSelector to dashboard

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -6,15 +6,21 @@ import { Routes, Route } from "react-router-dom";
 import Exercises from "./Exercises";
 import Overview from "./Overview";
 import Workout from "./workout/Workout";
+import { useSelector } from "react-redux";
+
 
 export default function Dashboard() {
+
+  const {userData} =useSelector( (state => state.auth)) 
+
+
   return (
     <div className="bg-[#2B2946] h-full flex w-full">
       <Sidebar />
       <div className="flex-col w-full ml-20">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Overview />} />
+          <Route path="/" element={<Overview user={userData} />} />
           <Route path="/exerciselist" element={<Exercises />} />
           <Route path="/workout" element={<Workout />} />
         </Routes>
