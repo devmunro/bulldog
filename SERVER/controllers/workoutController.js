0089c8393e8f750cfbf7571047c9db3cee26b689
David Munro <defmunro@gmail.com>
2023-02-07 14:11:38 +0000

    add workout id to user when created

--- SERVER/controllers/workoutController.js
+++ SERVER/controllers/workoutController.js
@@ -1,19 +1,29 @@
 import { Workout } from "../models/workoutSchema.js";
+import { user } from "../models/userSchema.js";
 
 export const createWorkout = async (req, res) => {
   const { userID, name } = req.body;
-  console.log(req.body)
-console.log(userID)
+
   try {
     const createUserWorkout = await Workout.create({
       userID,
       name,
     });
+
+    //adds workout id to user who created it
+    const userInfo = await user.findById(userID);
+    console.log("first")
+    userInfo.workouts.push(createUserWorkout._id);
+    console.log("second")
+    console.log("secondBBB")
+    await userInfo.save();
+    console.log("third")
+
     res.status(201).json({
       userID: createUserWorkout.userID,
       name: createUserWorkout.name,
     });
   } catch (error) {
-    res.status(400).json({ error: "workout not created" });
+    res.status(400).json({error });
   }
 };
