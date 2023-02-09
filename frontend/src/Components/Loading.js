1e68671bdbd7f2bf1d51b1d012c761186bb02e8a
David Munro <defmunro@gmail.com>
2023-02-09 18:16:32 +0000

    deploying to heroku

--- frontend/src/Components/Loading.js
+++ frontend/src/Components/Loading.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="flex justify-center mt-8 h-screen space-x-2">
+    <div className="flex justify-center mt-8 h-screen space-x-2 w-full">
       <ArrowPathIcon className="fill-white h-12 w-12 animate-spin" />
       <h2 className="text-white">LOADING...</h2>
     </div>
