d344f970224755d5a2db1e1b71d698e612b3bb6c
David Munro <defmunro@gmail.com>
2023-02-23 10:36:18 +0000

    add seperate exerciselist page

--- frontend/src/Components/Exercises.js
+++ frontend/src/Components/Exercises.js
@@ -3,7 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { addExercise } from "../features/exerciseSlice";
 import { MagnifyingGlassCircleIcon } from "@heroicons/react/24/solid";
 import ExerciseCatergories from "./exerciseCatergories";
-import Loading from "./Loading";
+
+import ExerciseList from "./ExerciseList";
 
 export default function Exercises() {
   const [exerciseList, setExerciseList] = useState();
@@ -89,96 +90,7 @@ setExerciseList(searchExercises)
         </button>
       </div>
       {/* // TABLE */}
-      <table className="table-auto w-full text-white text-justify align-middle">
-        <thead>
-          <tr className="[&>*]:p-4">
-            <th className="w-1/4">Name</th>
-            <th>Type</th>
-            <th>Equipment</th>
-            <th>Sets</th>
-            <th>Reps</th>
-            <th>Weight(KG)</th>
-            <th>Add to Workout</th>
-          </tr>
-        </thead>
-
-        {!loading &&
-          exerciseList.map((exercise) => {
-            return (
-              <tbody className="even:bg-[#7B7B8F] odd:bg-black ">
-                {/* EXERCISE NAME */}
-                <tr className="space-y-4 space-x-2 [&>*]:p-4">
-                  <td>{exercise.name}</td>
-
-                  {/* Exercise BODY TYPE */}
-
-                  <td>
-                    {exercise.body_type
-                      .map((type) => type.toUpperCase())
-                      .join(", ")}
-                  </td>
-
-                  {/* Exercise EQUIPMENT */}
-
-                  <td>
-                    {" "}
-                    {exercise.equipment
-                      .map((type) => type.toUpperCase())
-                      .join(", ")}
-                  </td>
-
-                  {/* SETS  */}
-
-                  <td>
-                    <input
-                      placeholder="3"
-                      className="w-12 h-12  text-center text-xl text-black bg-slate-400 focus:bg-slate-100"
-                      value={exerciseInputs[exercise._id]?.sets ?? 3}
-                      name="sets"
-                      onChange={handleChange(exercise._id)}
-                    ></input>
-                  </td>
-
-                  {/* REPS */}
-
-                  <td>
-                    <input
-                      placeholder="8"
-                      className="w-12 h-12 text-center text-xl text-black bg-slate-400 focus:bg-slate-100"
-                      value={exerciseInputs[exercise._id]?.reps ?? 8}
-                      name="reps"
-                      onChange={handleChange(exercise._id)}
-                    ></input>
-                  </td>
-
-                  {/* WEIGHTS KG */}
-
-                  <td>
-                    <input
-                      placeholder="10"
-                      className="w-12 h-12 text-center text-xl text-black bg-slate-400 focus:bg-slate-100"
-                      value={exerciseInputs[exercise._id]?.weight ?? 10}
-                      name="weight"
-                      onChange={handleChange(exercise._id)}
-                    ></input>
-                  </td>
-
-                  {/* ADD TO WORKOUT BUTTTON */}
-
-                  <td className="justify-center">
-                    <button
-                      className="bg-white text-black p-2 h-10 "
-                      onClick={() => handleAddToWorkout(exercise._id)}
-                    >
-                      Add
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
-            );
-          })}
-        {loading && <Loading />}
-      </table>
+     <ExerciseList loading={loading} handleAddToWorkout={handleAddToWorkout} exerciseInputs={exerciseInputs} exerciseList={exerciseList} handleChange={handleChange}/>
     </div>
   );
 }
