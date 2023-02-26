312e627f3dba999a4e75f57f06c58825749c28c8
David Munro <defmunro@gmail.com>
2023-02-26 17:23:42 +0000

    reg and login updated

--- frontend/src/Components/UserForms/userRegistration.js
+++ frontend/src/Components/UserForms/userRegistration.js
@@ -60,127 +60,131 @@ const RegisterForm = () => {
     <div>
       {error && (
         <div
-          className="flex items-center justify-center w-1/2 lg:w-1/3 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded "
+          className="flex items-center justify-center w-full bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded "
           role="alert"
         >
           <strong className="font-bold space-x-2">{error}</strong>
         </div>
       )}
-      <div className="w-full md:px-2 lg:px-4">
+      <div className="w-full md:p-4 lg:px-8 bg-black border-2 border-white ">
         <div className="w-full flex flex-col items-center">
           <h2 className="text-3xl lg:text-4xl font-bold text-white mb-2">
             Sign Up Here
           </h2>
-          <p className="text-gray-300 text-lg mb-6">
+          <p className="text-gray-300 text-lg mb-">
             Create your account in just a few clicks.
           </p>
 
-          <form
-            className="flex flex-col space-y-4 w-full mx-auto p-4 bg-gray-800 rounded-md border-2 border-gray-300"
-            onSubmit={handleSubmit}
-          >
-            <div className="flex flex-col">
-              <label htmlFor="name" className="text-white mb-2">
-                Name
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Name"
-                value={formData.name}
-                onChange={handleInputChange}
-                required
-                className="bg-gray-800 p-2 rounded-md border-2 border-gray-300 text-gray-300 focus:outline-none focus:border-purple-600"
-              />
+          <form className="w-full mx-auto p-4 bg-gray-800 rounded-md border-2 border-gray-300 ">
+            <div className="grid grid-cols-1 gap-4 ">
+              <div>
+                <label htmlFor="name" className="block text-white mb-2">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  required
+                  className="w-full py-2 px-3 bg-gray-800 border border-gray-300 rounded-md shadow-sm text-gray-300 focus:outline-none focus:border-purple-600 focus:ring focus:ring-purple-600 focus:ring-opacity-50"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="dob" className="block text-white mb-2">
+                  Date of Birth
+                </label>
+                <input
+                  type="date"
+                  name="dob"
+                  id="dob"
+                  placeholder="Date of Birth"
+                  value={formData.dob}
+                  onChange={handleInputChange}
+                  required
+                  className="w-full py-2 px-3 bg-gray-800 border border-gray-300 rounded-md shadow-sm text-gray-300 focus:outline-none focus:border-purple-600 focus:ring focus:ring-purple-600 focus:ring-opacity-50"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="email" className="block text-white mb-2">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  required
+                  className="w-full py-2 px-3 bg-gray-800 border border-gray-300 rounded-md shadow-sm text-gray-300 focus:outline-none focus:border-purple-600 focus:ring focus:ring-purple-600 focus:ring-opacity-50"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="password" className="block text-white mb-2">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  required
+                  className="w-full py-2 px-3 bg-gray-800 border border-gray-300 rounded-md shadow-sm text-gray-300 focus:outline-none focus:border-purple-600 focus:ring focus:ring-purple-600 focus:ring-opacity-50"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="passwordConfirmation"
+                  className="block text-white mb-2"
+                >
+                  Password Confirmation
+                </label>
+                <input
+                  type="password"
+                  name="passwordConfirmation"
+                  id="passwordConfirmation"
+                  placeholder="Password Confirmation"
+                  value={formData.passwordConfirmation}
+                  onChange={handleInputChange}
+                  required
+                  className="w-full py-2 px-3 bg-gray-800 border border-gray-300 rounded-md shadow-sm text-gray-300 focus:outline-none focus:border-purple-600 focus:ring focus:ring-purple-600 focus:ring-opacity-50"
+                />
+              </div>
             </div>
 
-            <div className="flex flex-col">
-              <label htmlFor="dob" className="text-white mb-2">
-                Date of Birth
-              </label>
-              <input
-                type="date"
-                name="dob"
-                id="dob"
-                placeholder="Date of Birth"
-                value={formData.dob}
-                onChange={handleInputChange}
-                required
-                className="bg-gray-800 p-2 rounded-md border-2 border-gray-300 text-gray-300 focus:outline-none focus:border-purple-600"
-              />
+            <div className="mt-6 w-full">
+              {!loading ? (
+                <button onClick={handleSubmit} className="btn-primary w-full" type="submit">
+                  Register
+                </button>
+              ) : (
+                <button
+                  disabled
+                  className=" w-full items-center bg-blue-800 text-white font-bold"
+                  type="submit"
+                >
+                  <Loading text="REGISTERING" />
+                </button>
+              )}
             </div>
-
-            <div className="flex flex-col">
-              <label htmlFor="email" className="text-white mb-2">
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email"
-                value={formData.email}
-                onChange={handleInputChange}
-                required
-                className="bg-gray-800 p-2 rounded-md border-2 border-gray-300 text-gray-300 focus:outline-none focus:border-purple-600"
-              />
-            </div>
-
-            <div className="flex flex-col">
-              <label htmlFor="password" className="text-white mb-2">
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Password"
-                value={formData.password}
-                onChange={handleInputChange}
-                required
-                className="bg-gray-800 p-2 rounded-md border-2 border-gray-300 text-gray-300 focus:outline-none focus:border-purple-600"
-              />
-            </div>
-
-            <div className="flex flex-col">
-              <label htmlFor="passwordConfirmation" className="text-white mb-2">
-                Password Confirmation
-              </label>
-              <input
-                type="password"
-                name="passwordConfirmation"
-                id="passwordConfirmation"
-                placeholder="Password Confirmation"
-                value={formData.passwordConfirmation}
-                onChange={handleInputChange}
-                required
-                className="bg-gray-800 p-2 rounded-md border-2 border-gray-300 text-gray-300 focus:outline-none focus:border-purple-600"
-              />
-            </div>
-            {!loading ? (
-    <button className="btn-primary" type="submit">
-      Register
-    </button>
-  ) : (
-    <button
-      disabled
-      className="items-center bg-blue-800 text-white font-bold"
-      type="submit"
-    >
-      <Loading text="REGISTERING" />
-    </button>
-  )}
- 
-</form>
-<p className="w-full text-center pt-2 pb-4">
-    Already have an account?
-    <span>
-      <button onClick={handleClick} className="px-2 italic">
-        Click Here
-      </button>
-    </span>
-  </p>
+          </form>
+          <p className="w-full text-center pt-2 pb-4 text-white">
+            Already have an account?
+            <span>
+              <button onClick={handleClick} className="text-blue-400 px-2 italic">
+                Click Here
+              </button>
+            </span>
+          </p>
         </div>
       </div>
     </div>
