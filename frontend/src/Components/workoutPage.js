7d51848675f36ad562396a4912cdc4f3e14ed284
David Munro <defmunro@gmail.com>
2023-03-03 11:40:18 +0000

    added functionaility of completing workout

--- frontend/src/Components/workoutPage.js
+++ frontend/src/Components/workoutPage.js
@@ -1,89 +1,121 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
-const WorkoutPage = () => {
+function WorkoutPage() {
   const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
   const { currentWorkout } = useSelector((state) => state.fitness);
+  const currentExercise = currentWorkout.exercises;
+  const exerciseID = currentExercise[currentExerciseIndex].exercise;
+  const exerciseName = currentExercise[currentExerciseIndex].name;
+  const exerciseSets = currentExercise[currentExerciseIndex].sets;
+  const exerciseReps = currentExercise[currentExerciseIndex].reps;
+  const exerciseWeight = currentExercise[currentExerciseIndex].weight;
 
+  const [disabledRows, setDisabledRows] = useState([]);
+
+  //exercisedata
+
+  const [exerciseData, setExerciseData] = useState([
+    {
+      id: "",
+      name: "",
+      sets: [{ reps: "", weight: "" }],
+    },
+  ]);
+
+  const dispatch = useDispatch()
+
+  //handle inputs
+
+  const handleInputChange = (id) => (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setExerciseData((prevState) => {
+      const updatedExercise = {
+        id: exerciseID,
+        name: exerciseName,
+        sets: prevState[currentExerciseIndex]?.sets || [
+          { reps: "", weight: "" },
+        ],
+      };
+      updatedExercise.sets[id] = {
+        ...updatedExercise.sets[id],
+        [name]: parseInt(value),
+      };
+      const newState = [...prevState];
+      newState[currentExerciseIndex] = updatedExercise;
+      return newState;
+    });
+  };
+  console.log(exerciseID);
+  console.log(exerciseData);
+
+  const handleDone = (rowIndex) => (e) => {
+    e.preventDefault();
+
+    setDisabledRows([...disabledRows, rowIndex]);
+  };
+
+  const setAmount = [];
+  for (let i = 0; i < exerciseSets; i++) {
+    setAmount.push(
+      <div key={i} className="flex space-x-4">
+        <h3>Set {i + 1}</h3>
+        <input
+          placeholder={exerciseReps}
+          type="number"
+          name="reps"
+          value={exerciseData[currentExerciseIndex]?.sets[i]?.reps || ""}
+          onChange={handleInputChange(i)}
+          disabled={disabledRows.includes(i)}
+          required
+        ></input>
+        <input
+          placeholder={`${exerciseWeight}kg`}
+          name="weight"
+          value={exerciseData[currentExerciseIndex]?.sets[i]?.weight || ""}
+          type="number"
+          onChange={handleInputChange(i)}
+          disabled={disabledRows.includes(i)}
+          required
+        ></input>
+
+        <button className="btn-primary" onClick={handleDone(i)}>
+          DONE
+        </button>
+      </div>
+    );
+  }
+
+  // be able to change to next exercise
   const handleNextExercise = () => {
-    if (currentExerciseIndex === currentWorkout.exercises.length - 1) return;
-    setCurrentExerciseIndex(currentExerciseIndex + 1);
+    const newNumber = currentExerciseIndex + 1;
+    console.log(newNumber);
+    if (newNumber === currentExercise.length) {
+      return;
+    }
+    setCurrentExerciseIndex(newNumber);
+    setDisabledRows([]);
   };
 
-  const handlePrevExercise = () => {
-    if (currentExerciseIndex === 0) return;
-    setCurrentExerciseIndex(currentExerciseIndex - 1);
+  const handleCompleteWorkout = (e) => {
+    e.preventDefault();
+    dispatch(completeWorkout(exerciseData))
   };
-
-  const currentExercise = currentWorkout?.exercises[currentExerciseIndex];
-
-  const handleStore = (e) => {
-    e.preventDefaut();
-    console.log(e);
-  };
-console.log(currentWorkout)
-console.log(currentExercise)
-
   return (
     <div>
-      {currentExercise === undefined && <div>nothing</div>}
-      {currentExercise && currentWorkout && (
-        <div className="p-4">
-          <h1 className="text-3xl font-bold mb-4">{currentWorkout.name}</h1>
-          <div className="my-4">
-            <h3 className="text-xl font-bold mb-2">{currentExercise.name}</h3>
-            {(() => {
-              const sets = [];
-              for (let i = 0; i < currentExercise?.sets; i++) {
-                sets.push(
-                  <div key={i} className="flex items-center my-2">
-                    <p className="mr-4">Set {i + 1}</p>
-                    <input
-                      type="text"
-                      className="border-gray-300 rounded-md shadow-sm p-2 w-20 mr-4"
-                      placeholder="Reps"
-                    />
-                    <p>reps</p>
-                    <input
-                      type="text"
-                      className="border-gray-300 rounded-md shadow-sm p-2 w-40 mr-4"
-                      placeholder="Weight lifted"
-                    />
-                    <p className="mr-4">kg</p>
-                    <button
-                      className="bg-orange-400 text-gray-600 rounded-md py-2 px-4 hover:bg-gray-300"
-                      onClick={handleStore}
-                    >
-                      Done
-                    </button>
-                  </div>
-                );
-              }
-              return sets;
-            })()}
-          </div>
-          <div className="flex justify-between items-center">
-            <button
-              className="bg-gray-200 text-gray-600 rounded-md py-2 px-4 hover:bg-gray-300"
-              onClick={handlePrevExercise}
-            >
-              Previous Exercise
-            </button>
-            <p className="text-gray-600 font-bold">
-              Exercise {currentExerciseIndex + 1} of{" "}
-              {currentWorkout.exercises.length}
-            </p>
-            <button
-              className="bg-gray-200 text-gray-600 rounded-md py-2 px-4 hover:bg-gray-300"
-              onClick={handleNextExercise}
-            >
-              Next Exercise
-            </button>
-          </div>
-        </div>
-      )}
+      <form className="bg-white">
+        <h2>{exerciseName}</h2>
+        {setAmount}
+
+        <button type="button" onClick={handleNextExercise}>
+          Next Exercise
+        </button>
+      </form>
+
+      <button onClick={handleCompleteWorkout}>Complete</button>
     </div>
   );
-};
+}
 
 export default WorkoutPage;
