755eae978770acc38c43b7c4663327fa7982fe8a
David Munro <defmunro@gmail.com>
2023-03-03 12:40:19 +0000

    fix router error

--- SERVER/routes/workoutStatsRoutes.js
+++ SERVER/routes/workoutStatsRoutes.js
@@ -5,7 +5,7 @@ import { workoutCompleted } from "../controllers/workoutStatsController.js";
 export const WorkoutStatsRouter = () => {
   const workoutStatsRouter = express();
   // full route example /api/workoutStats/completeworkout
-  workoutRouter.post("/completeworkout", workoutCompleted); //
+  workoutStatsRouter.post("/completeworkout", workoutCompleted); //
 
 
   return workoutStatsRouter;
