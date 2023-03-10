b735102c1c82dd0c0d9388608eb8a8574a9685e7
David Munro <defmunro@gmail.com>
2023-03-10 14:38:09 +0000

    sorting out mobile design

--- frontend/src/Components/workoutPage.js
+++ frontend/src/Components/workoutPage.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { completeWorkout } from "../features/exerciseSlice";
+import { Link } from "react-router-dom";
 
 function WorkoutPage() {
   const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
   const { currentWorkout } = useSelector((state) => state.fitness);
   const { user } = useSelector((state) => state.auth);
-  const currentExercise = currentWorkout.exercises;
-  const exerciseID = currentExercise[currentExerciseIndex].exercise;
-  const exerciseName = currentExercise[currentExerciseIndex].name;
-  const exerciseSets = currentExercise[currentExerciseIndex].sets;
-  const exerciseReps = currentExercise[currentExerciseIndex].reps;
-  const exerciseWeight = currentExercise[currentExerciseIndex].weight;
+  const currentExercise = currentWorkout?.exercises;
+  const exerciseID = currentExercise?.[currentExerciseIndex]?.exercise;
+  const exerciseName = currentExercise?.[currentExerciseIndex]?.name;
+  const exerciseSets = currentExercise?.[currentExerciseIndex]?.sets;
+  const exerciseReps = currentExercise?.[currentExerciseIndex]?.reps;
+  const exerciseWeight = currentExercise?.[currentExerciseIndex]?.weight;
 
   const [disabledRows, setDisabledRows] = useState([]);
   const [showTimer, setShowTimer] = useState(false);
@@ -34,13 +35,13 @@ function WorkoutPage() {
   const handleInputChange = (id) => (e) => {
     e.preventDefault();
     const { name, value } = e.target;
-    console.log("here",e.target.name)
+    console.log("here", e.target.name);
     setExerciseData((prevState) => {
       const updatedExercise = {
         id: exerciseID,
         name: exerciseName,
         sets: prevState[currentExerciseIndex]?.sets || [
-          { reps: exerciseReps, weight:exerciseWeight },
+          { reps: exerciseReps, weight: exerciseWeight },
         ],
       };
       updatedExercise.sets[id] = {
@@ -66,8 +67,13 @@ function WorkoutPage() {
   const setAmount = [];
   for (let i = 0; i < exerciseSets; i++) {
     setAmount.push(
-      <div key={i} className="flex  flex-col md:flex-row md:space-x-4  justify-center">
-        <h3 className="mb-2 md:mb-0 text-center m-4 font-bold">Set {i + 1}</h3>
+      <div
+        key={i}
+        className="flex  md:flex-row md:space-x-4 items-center justify-center"
+      >
+        <h3 className="mb-2 md:mb-0 text-center md:m-4 font-bold">
+          Set {i + 1}
+        </h3>
         <input
           placeholder={exerciseReps}
           type="number"
@@ -75,7 +81,7 @@ function WorkoutPage() {
           value={exerciseData[currentExerciseIndex]?.sets[i]?.reps || ""}
           onChange={handleInputChange(i)}
           disabled={disabledRows.includes(i)}
-          className={`py-2 px-4 m-4 rounded-lg ${
+          className={`py-2 md:px-4 px-2 m-4 rounded-lg w-12 md:w-full text-center ${
             disabledRows.includes(i)
               ? "bg-gray-400 placeholder-white my-2 border-2"
               : "bg-white border-2 border-black my-2"
@@ -88,7 +94,7 @@ function WorkoutPage() {
           type="number"
           onChange={handleInputChange(i)}
           disabled={disabledRows.includes(i)}
-          className={`py-2 px-4 m-4 rounded-lg ${
+          className={`py-2 px-4 md:m-4 rounded-lg w-20 md:w-full text-center${
             disabledRows.includes(i)
               ? "bg-gray-400 placeholder-white my-2 border-2"
               : "bg-white border-2 border-black my-2"
@@ -101,7 +107,7 @@ function WorkoutPage() {
         >
           +
         </button>
-        <hr className="bg-black m-4"></hr>
+        <hr className="bg-black md:m-4"></hr>
       </div>
     );
   }
@@ -143,65 +149,88 @@ function WorkoutPage() {
       clearTimeout(timerId);
     };
   }, [showTimer, secondsLeft]);
-
+  console.log(currentWorkout.exercise);
   return (
     <section className="flex flex-col w-full justify-center">
-      <form className="bg-white rounded-lg shadow-lg p-8 lg:w-2/3 w-full lg:mx-auto">
-        <h2 className="text-2xl font-bold mb-4 text-center">{exerciseName}</h2>
-        {setAmount}
-        {showTimer && (
-          <div className="fixed w-full h-full top-0 left-0 z-50 flex items-center justify-center">
-            <div className="absolute inset-0 bg-black opacity-80" />
+      <h2 className="uppercase text-center m-4 p-4 bg-white -">
+        {currentWorkout.name}
+      </h2>
 
-            <div className="flex justify-center md:w-1/2 w-full h-1/2 items-center z-10 px-8 text-4xl  bg-black font-bold">
-              <div className="flex justify-center">
-                {showTimer && secondsLeft !== 0 && (
-                  <div className="text-center my-4  w-full h-full">
-                    <h3 className="font-bold text-red-500">
-                      <p>REST</p>
-                      <br />
-                      {secondsLeft}
-                      <span>secs</span>
-                    </h3>
-                  </div>
-                )}
-                {showTimer && secondsLeft === 0 && (
-                  <div className="text-center my-4">
-                    <h3 className="  text-red-500">Time's up!</h3>
-                    <button
-                      onClick={() => {
-                        setShowTimer(false);
-                      }}
-                      className="btn-primary"
-                    >
-                      NEXT SET
-                    </button>
-                  </div>
-                )}
-              </div>
-            </div>
+      {/* NO WORKOUT SECTION */}
+      {!currentWorkout && <p>Loading...</p>}
+      {!currentWorkout.exercise && (
+        <div className="bg-white p-4 flex flex-col w-max m-4">
+          <div className="justify-center ">
+            <p >
+              No Exercises found, please add exercises to start
+            </p>
+            <Link to="/dashboard/exerciselist"> Click Here to Start</Link>
           </div>
-        )}
-      </form>
-      <button
-        type="button"
-        onClick={() => {
-          setShowTimer(false);
-          setSecondsLeft(45);
-          handleNextExercise();
-        }}
-        className={`${
-          showTimer && secondsLeft > 0 ? "hidden" : "block"
-        } btn-primary lg:w-2/3 w-full lg:mx-auto`}
-      >
-        Next Exercise
-      </button>
-      <button
-        onClick={handleCompleteWorkout}
-        className="lg:w-2/3 w-full lg:mx-auto bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-4"
-      >
-        Complete
-      </button>
+        </div>
+      )}
+      {/* WORKOUT SECTION */}
+      {currentWorkout.exercises > 0 && (
+        <div>
+          <form className="bg-white rounded-lg shadow-lg p-8 lg:w-2/3 w-full lg:mx-auto">
+            <h2 className="md:text-2xl text-l font-bold md:mb-4 text-center">
+              {exerciseName}
+            </h2>
+            {setAmount}
+            {showTimer && (
+              <div className="fixed w-full h-full top-0 left-0 z-50 flex items-center justify-center">
+                <div className="absolute inset-0 bg-black opacity-80" />
+
+                <div className="flex justify-center md:w-1/2 w-full h-1/2 items-center z-10 px-8 text-4xl  bg-black font-bold">
+                  <div className="flex justify-center">
+                    {showTimer && secondsLeft !== 0 && (
+                      <div className="text-center my-4  w-full h-full">
+                        <h3 className="font-bold text-red-500">
+                          <p>REST</p>
+                          <br />
+                          {secondsLeft}
+                          <span>secs</span>
+                        </h3>
+                      </div>
+                    )}
+                    {showTimer && secondsLeft === 0 && (
+                      <div className="text-center my-4">
+                        <h3 className="  text-red-500">Time's up!</h3>
+                        <button
+                          onClick={() => {
+                            setShowTimer(false);
+                          }}
+                          className="btn-primary"
+                        >
+                          NEXT SET
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            )}
+          </form>
+          <button
+            type="button"
+            onClick={() => {
+              setShowTimer(false);
+              setSecondsLeft(45);
+              handleNextExercise();
+            }}
+            className={`${
+              showTimer && secondsLeft > 0 ? "hidden" : "block"
+            } btn-primary lg:w-2/3 w-full lg:mx-auto`}
+          >
+            Next Exercise
+          </button>
+          <button
+            onClick={handleCompleteWorkout}
+            className="lg:w-2/3 w-full lg:mx-auto bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-4"
+          >
+            Complete
+          </button>
+        </div>
+      )}
     </section>
   );
 }
