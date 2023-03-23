0f7a36e972bf5f0f4bd0decc07b94ed5961de615
David Munro <defmunro@gmail.com>
2023-03-23 13:12:10 +0000

    sorted out currentWorkout not loading

--- bulldogapp/src/home.js
+++ bulldogapp/src/home.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import {clearState, getUserDetails, loginUser} from './features/userSlice';
-import {useDispatch, useSelector} from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { clearState, getUserDetails, loginUser } from './features/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   Keyboard,
   ScrollView,
@@ -11,35 +11,41 @@ import {
   View,
 } from 'react-native';
 import DashboardNavigate from './Navigate/dashboardNavigate';
-import {findSingleWorkout} from './features/exerciseSlice';
+import { findSingleWorkout } from './features/exerciseSlice';
 
 export default function Dashboard() {
   const dispatch = useDispatch();
+  
   useEffect(() => {
     const getUser = async () => {
       const getDetails = await dispatch(getUserDetails());
     };
-    const getCurrentworkout = async () => {
-      const getWorkout = await dispatch(findSingleWorkout(user.defaultWorkout));
-      console.log(getWorkout);
-      console.log(currentWorkout);
-    };
 
     getUser();
-    if (user) {
-      getCurrentworkout();
-    }
   }, []);
 
-  const {user} = useSelector(state => state.auth);
-  const {currentWorkout} = useSelector(state => state.fitness);
-console.log(currentWorkout)
+  useEffect(() => {
+    const getCurrentWorkout = async () => {
+      if (user && user.defaultWorkout) {
+        const getWorkout = await dispatch(findSingleWorkout(user.defaultWorkout));
+        console.log(getWorkout);
+        console.log(currentWorkout);
+      }
+    };
+
+    getCurrentWorkout();
+  }, [user]);
+
+  const { user } = useSelector((state) => state.auth);
+  const { currentWorkout } = useSelector((state) => state.fitness);
+  console.log(currentWorkout);
+
   return (
-    <View >
+    <View>
       {user && currentWorkout && (
         <View>
-          <Text >hello {user.name}</Text>
-          <Text >Your workout is {currentWorkout.name}</Text>
+          <Text>hello {user.name}</Text>
+          <Text>Your workout is {currentWorkout.name}</Text>
         </View>
       )}
     </View>
