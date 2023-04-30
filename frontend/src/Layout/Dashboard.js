b339b1f905be62fd4646a81e4cbb7d603ecc833e
David Munro <defmunro@gmail.com>
2023-04-30 16:08:55 +0100

    updating layout

--- frontend/src/Layout/Dashboard.js
+++ frontend/src/Layout/Dashboard.js
@@ -4,7 +4,7 @@ import Navbar from "./Navbar"; // import the Navbar component
 import { Routes, Route, useNavigate } from "react-router-dom"; // import necessary components from react-router-dom
 import Exercises from "../Components/Exercise/Exercises"; // import the Exercises component
 import Overview from "../Components/Overview"; // import the Overview component
-import Workout from "../Components/workout/WorkoutManager";// import the Workout component
+import Workout from "../Components/workout/WorkoutManager"; // import the Workout component
 import WorkoutPage from "../Components/workout/workoutPage"; // import the WorkoutPage component
 import { useDispatch, useSelector } from "react-redux"; // import useDispatch and useSelector from react-redux
 import { getUserDetails, logout } from "../features/userSlice"; // import getUserDetails and logout from ../features/userSlice
@@ -16,7 +16,7 @@ export default function Dashboard() {
   const dispatch = useDispatch(); // use dispatch to dispatch actions to the store
   const navigate = useNavigate(); // useNavigate is a hook that returns a navigate function to navigate to a different route
 
-  console.log(user)
+  console.log(user);
   //check if authorised
   useEffect(() => {
     if (token === null) {
@@ -34,7 +34,7 @@ export default function Dashboard() {
   const handleLogout = (e) => {
     e.preventDefault(); // prevent the default behaviour of the form
     dispatch(logout()); // dispatch the logout action to log the user out
-    dispatch(resetWorkout())
+    dispatch(resetWorkout());
   };
 
   return (
@@ -43,16 +43,16 @@ export default function Dashboard() {
         <Loading />
       ) : (
         <>
-          <div className="fixed left-0 top-0 w-10">
+          <div className="bg-primary flex">
             <Sidebar handleLogout={handleLogout} />
-          </div>
-          <div className="flex-grow flex flex-col ml-10 md:ml-14 bg-primary">
-             <Routes>
-              <Route path="/" element={<Overview user={user} />} />
-              <Route path="/exerciselist" element={<Exercises />} />
-              <Route path="/workout" element={<Workout user={user} />} />
-              <Route path="/record" element={<WorkoutPage user={user} />} />
-            </Routes>
+            <div className="bg-secondary m-4">
+              <Routes>
+                <Route path="/" element={<Overview user={user} />} />
+                <Route path="/exerciselist" element={<Exercises />} />
+                <Route path="/workout" element={<Workout user={user} />} />
+                <Route path="/record" element={<WorkoutPage user={user} />} />
+              </Routes>
+            </div>
           </div>
         </>
       )}
--- frontend/src/Layout/Sidebar.js
+++ frontend/src/Layout/Sidebar.js
@@ -21,92 +21,78 @@ export default function Sidebar() {
 
   return (
     <div className="fixed min-h-screen flex-col bg-primary">
-      <div className="flex justify-between">
-        <button className="mt-6 p-4 m-2  text-secondary hover:bg-secondary hover:text-tertiary rounded-2xl " onClick={handleSidebar}>
-          {hideSidebar && (
-            <ChevronDoubleRightIcon className="md:h-6 md:w-6 h-5 w-5 md:mx-2  " />
-          )}
-          {!hideSidebar && (
-            <ChevronDoubleLeftIcon className="md:h-6 md:w-6 h-5 w-5  md:mx-2 hover:bg-secondary hover:text-tertiary" />
-          )}
-        </button>
-      </div>
-
-      {!hideSidebar && (
-        <div className=" ">
-          <ul className="mt-16 space-y-16 text-sm text-center text-secondary [&>*]:p-4 [&>*]:m-2 [&>*]:rounded-2xl [&>*]:flex  ">
-            <li className="hover:bg-secondary hover:text-tertiary">
-              <Link to="/dashboard" className="flex">
-                <ComputerDesktopIcon className="md:h-6 md:w-6 h-5 w-5 md:mx-2 mx-1" />
-                Overview
-              </Link>
-            </li>
-
-            <li className="hover:bg-secondary hover:text-tertiary">
-              <Link to="/dashboard/exerciselist" className="flex">
-                <BookOpenIcon className="md:h-6 md:w-6 h-5 w-5 md:mx-2 mx-1" />
-                Exercises
-              </Link>
-            </li>
-            <li className="hover:bg-secondary hover:text-tertiary">
-              <Link to="/dashboard/workout" className="flex">
-                <DocumentTextIcon className="md:h-6 md:w-6 h-5 w-5 md:mx-2 mx-1" />
-                Workout Planner
-              </Link>
-            </li>
-            <li className="hover:bg-secondary hover:text-tertiary">
-              <Link to="/dashboard/record" className="flex">
-                <TrophyIcon className="md:h-6 md:w-6 h-5 w-5 md:mx-2 mx-1" />
-                Start Workout
-              </Link>
-            </li>
-            <li className="hover:bg-secondary hover:text-tertiary">
-              <Link to="/dashboard/record" className="flex">
-                <BackspaceIcon className="md:h-6 md:w-6 h-5 w-5 md:mx-2 mx-1" />
-                Logout
-              </Link>
-            </li>
-            {/* <li className="hover:bg-[#2B2946] hover:text-white">
-          <UsersIcon className="h-6 w-6 text-white-500 mx-2" />
-          Social
-        </li> */}
-            {/* <li className="hover:bg-[#2B2946] hover:text-white">
-          <ChartBarIcon className="h-6 w-6 text-white-500 mx-2" />
-          Stats
-        </li> */}
-          </ul>
-        </div>
-      )}
-      {hideSidebar && (
-        <ul className="mt-16 space-y-16 text-sm text-center text-secondary [&>*]:p-4 [&>*]:m-2 [&>*]:rounded-2xl [&>*]:flex ">
+   
+        <ul className="  sm:block hidden  mt-16 space-y-16 text-sm text-center text-secondary [&>*]:p-4 [&>*]:m-2 [&>*]:rounded-2xl [&>*]:flex  ">
           <li className="hover:bg-secondary hover:text-tertiary">
-            <Link to="/dashboard">
-              <ComputerDesktopIcon className="md:h-6 md:w-6 h-5 w-5  md:mx-2" />
+            <Link to="/dashboard" className="flex">
+              <ComputerDesktopIcon className="h-6 w-6 mx-2 " />
+              Overview
             </Link>
           </li>
 
           <li className="hover:bg-secondary hover:text-tertiary">
-            <Link to="/dashboard/exerciselist">
-              <BookOpenIcon className="md:h-6 md:w-6 h-5 w-5  md:mx-2" />
+            <Link to="/dashboard/exerciselist" className="flex">
+              <BookOpenIcon className="h-6 w-6 mx-2 " />
+              Exercises
             </Link>
           </li>
           <li className="hover:bg-secondary hover:text-tertiary">
-            <Link to="/dashboard/workout">
-              <DocumentTextIcon className="md:h-6 md:w-6 h-5 w-5  md:mx-2" />
-            </Link>
-          </li>
-          <li className="hover:bg-secondary hover:text-tertiarye">
-            <Link to="/dashboard/record">
-              <TrophyIcon className="md:h-6 md:w-6 h-5 w-5 md:mx-2" />
+            <Link to="/dashboard/workout" className="flex">
+              <DocumentTextIcon className="h-6 w-6 mx-2 " />
+              Workout Planner
             </Link>
           </li>
           <li className="hover:bg-secondary hover:text-tertiary">
             <Link to="/dashboard/record" className="flex">
-              <BackspaceIcon className="md:h-6 md:w-6 h-5 w-5 md:mx-2 mx-1" />
+              <TrophyIcon className="h-6 w-6 mx-2 " />
+              Start Workout
             </Link>
           </li>
+          <li className="hover:bg-secondary hover:text-tertiary">
+            <Link to="/dashboard/record" className="flex">
+              <BackspaceIcon className="h-6 w-6 mx-2 " />
+              Logout
+            </Link>
+          </li>
+          {/* <li className="hover:bg-[#2B2946] hover:text-white">
+          <UsersIcon className="h-6 w-6 text-white-500 mx-2" />
+          Social
+        </li> */}
+          {/* <li className="hover:bg-[#2B2946] hover:text-white">
+          <ChartBarIcon className="h-6 w-6 text-white-500 mx-2" />
+          Stats
+        </li> */}
         </ul>
-      )}
+      
+
+      <ul className=" block sm:hidden mt-16 space-y-16 text-sm text-center text-secondary [&>*]:p-4 [&>*]:m-2 [&>*]:rounded-2xl [&>*]:flex ">
+        <li className="hover:bg-secondary hover:text-tertiary">
+          <Link to="/dashboard">
+            <ComputerDesktopIcon className="md:h-6 md:w-6 h-5 w-5  md:mx-2" />
+          </Link>
+        </li>
+
+        <li className="hover:bg-secondary hover:text-tertiary">
+          <Link to="/dashboard/exerciselist">
+            <BookOpenIcon className="md:h-6 md:w-6 h-5 w-5  md:mx-2" />
+          </Link>
+        </li>
+        <li className="hover:bg-secondary hover:text-tertiary">
+          <Link to="/dashboard/workout">
+            <DocumentTextIcon className="md:h-6 md:w-6 h-5 w-5  md:mx-2" />
+          </Link>
+        </li>
+        <li className="hover:bg-secondary hover:text-tertiarye">
+          <Link to="/dashboard/record">
+            <TrophyIcon className="md:h-6 md:w-6 h-5 w-5 md:mx-2" />
+          </Link>
+        </li>
+        <li className="hover:bg-secondary hover:text-tertiary">
+          <Link to="/dashboard/record" className="flex">
+            <BackspaceIcon className="md:h-6 md:w-6 h-5 w-5 md:mx-2 mx-1" />
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
