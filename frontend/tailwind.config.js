3bf3910c3256fd6944936f8a04331651fe894e59
David Munro <defmunro@gmail.com>
2023-04-30 06:57:34 +0100

    add adjustments to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    
+    backgroundColor: {
+      primary: '#4F46E5',
+      secondary: '#BFDBFE',
+      tertiary: '#111827',
+    },
     extend: {},
   },
   plugins: [],
-}
+};
