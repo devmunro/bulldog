af48f0e9e5c5590d321a2025d32fd86b810c9afa
devmunro <defmunro@gmail.com>
2023-06-09 22:05:32 +0100

    updated store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from '../features/userSlice'
 import exerciseReducer from '../features/exerciseSlice'
+import { bodyStatSlice } from '../features/bodySlice'
 
 export const store = configureStore({
 
     reducer: {
         auth: userReducer,
-        fitness: exerciseReducer
+        fitness: exerciseReducer,
+        body: bodyStatSlice
     }
 })
