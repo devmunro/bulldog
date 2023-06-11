585731adf21f457a94c2450899f7f62df167ef33
devmunro <defmunro@gmail.com>
2023-06-11 17:09:02 +0100

    updated backed

--- SERVER/controllers/bodyStatController.js
+++ SERVER/controllers/bodyStatController.js
@@ -3,8 +3,6 @@ import { BodyStat } from "../models/bodyStatSchema.js";
 
 export const getBodyWeight = async (req, res) => {
   const userID = req.query.userID;
-  console.log("BODYUSER", userID);
-
   try {
     const bodyStats = await BodyStat.find({ userID: userID });
     console.log("BODYUSER", bodyStats);
@@ -17,19 +15,31 @@ export const getBodyWeight = async (req, res) => {
 
 export const addBodyWeight = async (req, res) => {
   const { userID, weight } = req.body;
-  console.log("BODYUSER", userID);
 
   try {
+
+ // Check if a record already exists for the current day
+ const existingEntry = await BodyStat.findOne({
+    userID,
+    createdAt: {
+      $gte: new Date(new Date().setHours(0, 0, 0, 0)), // beginning of today
+      $lte: new Date(new Date().setHours(23, 59, 59, 999)) // end of today
+    }
+  });
+
+  if (existingEntry) {
+    return res.status(400).json({ error: "You have already submitted your weight for today" });
+  }
+
+
     const createBodyWeightStat = await BodyStat.create({
       userID,
       weight,
-      date: createBodyWeightStat.createdAt,
     });
 
     res.status(201).json({
       userID: createBodyWeightStat.userID,
       weight: createBodyWeightStat.weight,
-      date: createBodyWeightStat.createdAt,
     });
   } catch (error) {
     console.log(error);
